97293a41bbf47fcec0db1a32c6c50694238e3d4c
Zehir <zehir@zorim.fr>
2016-01-20 15:50:37 +0100

    Add French Translation

---
+++ src/Language/French/Dictionary/Number.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Kwn\NumberToWords\Language\English\Dictionary;
+
+class Number
+{
+    protected $zero = 'zéro';
+
+    protected $units = ['', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf'];
+
+    protected $teens = ['dix', 'onze', 'douze', 'treize', 'quatorze', 'quinze',
+        'seize', 'dix-sept', 'dix-huit', 'dix-neuf'];
+
+    protected $tens = ['', 'dix', 'vingt', 'trente', 'quarante', 'cinquante',
+        'soixante', 'soixante-dix', 'quatre-vingts', 'quatre-vingts-dix'];
+
+    protected $hundred = 'cent';
+
+    protected $mega = ['', 'millier', 'million', 'milliard', 'billion', 'quadrillion', 'quintillion', 'sextillion'];
+
+    /**
+     * @return string
+     */
+    public function getZero()
+    {
+        return $this->zero;
+    }
+
+    /**
+     * @param int $value
+     *
+     * @return string
+     */
+    public function getUnit($value)
+    {
+        return $this->units[$value];
+    }
+
+    /**
+     * @param int $tens
+     * @param int $units
+     *
+     * @return string
+     */
+    public function getSubHundred($tens, $units)
+    {
+        $words = [];
+
+        if ($tens === 1) {
+            $words[] = $this->teens[$units];
+        } else {
+            if ($tens > 0) {
+                $words[] = $this->tens[$tens];
+            }
+            if ($units > 0) {
+                $words[] = $this->units[$units];
+            }
+        }
+
+        return implode(' ', $words);
+    }
+
+    /**
+     * @param int $value
+     *
+     * @return string
+     */
+    public function getHundred($value)
+    {
+        if ($word = $this->getUnit($value)) {
+            return $word . ' ' . $this->hundred;
+        }
+    }
+
+    /**
+     * @param int $scale
+     *
+     * @return string
+     */
+    public function getMega($scale)
+    {
+        if ($word = $this->mega[$scale]) {
+            return $word;
+        }
+    }
+}
---
+++ src/Language/French/Transformer/CurrencyTransformer.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Kwn\NumberToWords\Language\English\Transformer;
+
+use Kwn\NumberToWords\Model\Number;
+use Kwn\NumberToWords\Model\SubunitFormat;
+use Kwn\NumberToWords\Language\English\Dictionary\Currency as CurrencyDictionary;
+use Kwn\NumberToWords\Transformer\CurrencyTransformer as BaseCurrencyTransformer;
+
+class CurrencyTransformer extends BaseCurrencyTransformer
+{
+    /**
+     * @var NumberTransformer
+     */
+    protected $numberTransformer;
+
+    /**
+     * @var CurrencyDictionary
+     */
+    protected $currencyDictionary;
+
+    /**
+     * @param NumberTransformer $numberTransformer
+     * @param CurrencyDictionary $currencyDictionary
+     */
+    public function __construct(NumberTransformer $numberTransformer, CurrencyDictionary $currencyDictionary)
+    {
+        $this->numberTransformer = $numberTransformer;
+        $this->currencyDictionary = $currencyDictionary;
+    }
+
+    /**
+     * Convert given number to words
+     *
+     * @param mixed $number
+     *
+     * @return string
+     */
+    public function toWords($number)
+    {
+        $number = $this->createCurrencyNumber($number);
+
+        return $this->getIntegerPart($number) . ' ' . $this->getFractionalPart($number);
+    }
+
+    /**
+     * Gets an array of valid currencies (ISO 4217)
+     *
+     * @return array
+     */
+    protected function getValidCurrencies()
+    {
+        return array_keys($this->currencyDictionary->getUnitNames());
+    }
+
+    /**
+     * Gets the integer part of the provided amount
+     *
+     * @param Number $number
+     *
+     * @return string
+     */
+    protected function getIntegerPart(Number $number)
+    {
+        $value = $number->getValue();
+        $unitName = $this->currencyDictionary->getUnitName($this->currency, !$this->isSingular($value));
+
+        return $this->numberTransformer->toWords($value) . ' ' . $unitName;
+    }
+
+    /**
+     * Gets the fractional part of the provided amount
+     *
+     * @param Number $number
+     *
+     * @return string
+     */
+    protected function getFractionalPart(Number $number)
+    {
+        $subunitValue = $number->getSubunits();
+
+        //if the subunit format is numbers, we want to simply return a fraction
+        if ($this->subunitFormat->getFormat() === SubunitFormat::NUMBERS) {
+            return $subunitValue . '/100';
+        }
+
+        $unitName = $this->currencyDictionary->getSubunitName($this->currency, !$this->isSingular($subunitValue));
+
+        return $this->numberTransformer->toWords($subunitValue) . ' ' . $unitName;
+    }
+
+    /**
+     * Determines if the provided number is singular
+     *
+     * @param mixed $number
+     *
+     * @return bool
+     */
+    protected function isSingular($number)
+    {
+        return $number == 1;
+    }
+}
