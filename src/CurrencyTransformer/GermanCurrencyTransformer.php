b9dafec8dab21516a7634b0752a2de3dc716971f
Andreas Lemke <andreas.lemke@tu-braunschweig.de>
2017-11-26 12:11:48 +0100

    Update GermanCurrencyTransformer.php

--- src/CurrencyTransformer/GermanCurrencyTransformer.php
+++ src/CurrencyTransformer/GermanCurrencyTransformer.php
@@ -2,7 +2,12 @@
 
 namespace NumberToWords\CurrencyTransformer;
 
-use NumberToWords\Legacy\Numbers\Words;
+use NumberToWords\Exception\NumberToWordsException;
+use NumberToWords\Language\German\GermanDictionary;
+use NumberToWords\Language\German\GermanExponentGetter;
+use NumberToWords\Language\German\GermanTripletTransformer;
+use NumberToWords\NumberTransformer\NumberTransformerBuilder;
+use NumberToWords\Service\NumberToTripletsConverter;
 
 class GermanCurrencyTransformer implements CurrencyTransformer
 {
@@ -10,12 +15,69 @@ class GermanCurrencyTransformer implements CurrencyTransformer
      * @param int    $amount
      * @param string $currency
      *
+     * @throws NumberToWordsException
      * @return string
      */
     public function toWords($amount, $currency)
     {
-        $converter = new Words();
+        $dictionary = new GermanDictionary();
+        $numberToTripletsConverter = new NumberToTripletsConverter();
+        $tripletTransformer = new GermanTripletTransformer($dictionary);
+        $exponentInflector = new GermanExponentGetter();
 
-        return $converter->transformToCurrency($amount, 'de', $currency);
+        $numberTransformer = (new NumberTransformerBuilder())
+            ->withDictionary($dictionary)
+            ->withWordsSeparatedBy(' ')
+            ->transformNumbersBySplittingIntoTriplets($numberToTripletsConverter, $tripletTransformer)
+            ->useRegularExponents($exponentInflector)
+            ->build();
+
+        $decimal = (int) ($amount / 100);
+        $fraction = $amount % 100;
+
+        if ($fraction === 0) {
+            $fraction = null;
+        }
+
+        $currency = strtoupper($currency);
+
+        if (!array_key_exists($currency, GermanDictionary::$currencyNames)) {
+            throw new NumberToWordsException(
+                sprintf('Currency "%s" is not available for "%s" language', $currency, get_class($this))
+            );
+        }
+
+        $currencyNames = GermanDictionary::$currencyNames[$currency];
+
+        $return = trim($numberTransformer->toWords($decimal));
+        $level = ($decimal === 1) ? 0 : 1;
+
+        if ($level > 0) {
+            if (count($currencyNames[0]) > 1) {
+                $return .= ' ' . $currencyNames[0][$level];
+            } else {
+                $return .= ' ' . $currencyNames[0][0] . 's';
+            }
+        } else {
+            $return .= ' ' . $currencyNames[0][0];
+        }
+
+        if (null !== $fraction) {
+            $return .= ' ' . trim($numberTransformer->toWords($fraction));
+
+            $level = $fraction === 1 ? 0 : 1;
+
+            if ($level > 0) {
+                if (count($currencyNames[1]) > 1) {
+                    $return .= ' ' . $currencyNames[1][$level];
+                } else {
+                    $return .= ' ' . $currencyNames[1][0] . 's';
+                }
+            } else {
+                $return .= ' ' . $currencyNames[1][0];
+            }
+        }
+
+        return $return;
     }
 }
