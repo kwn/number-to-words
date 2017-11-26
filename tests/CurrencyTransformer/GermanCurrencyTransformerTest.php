b9c4ad0c2203d519aef5875e3c4eeb7c5771e0fa
Andreas Lemke <andreas.lemke@tu-braunschweig.de>
2017-11-26 13:12:28 +0100

    Update GermanCurrencyTransformerTest.php

--- tests/CurrencyTransformer/GermanCurrencyTransformerTest.php
+++ tests/CurrencyTransformer/GermanCurrencyTransformerTest.php
@@ -12,10 +12,7 @@ class GermanCurrencyTransformerTest extends CurrencyTransformerTest
     public function providerItConvertsMoneyAmountToWords()
     {
         return [
-            [100, 'DKK', 'en krone'],
-            [200, 'USD', 'to dollars'],
-            [600, 'EUR', 'seks euro'],
-            [1300, 'CHF', 'tretten schweitzer franc'],
+            [600, 'EUR', 'sechshundert Euro']
         ];
     }
 }
