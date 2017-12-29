6b44d2c77aedbb5e2517862160f393db4c000f23
andreybolonin <andreybolonin1989@gmail.com>
2017-12-30 01:37:34 +0200

    add UkrainianCurrencyTransformer

---
+++ src/CurrencyTransformer/UkrainianCurrencyTransformer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace NumberToWords\CurrencyTransformer;
+
+use NumberToWords\Legacy\Numbers\Words;
+
+class RussianCurrencyTransformer implements CurrencyTransformer
+{
+    /**
+     * @param int    $amount
+     * @param string $currency
+     *
+     * @return string
+     */
+    public function toWords($amount, $currency)
+    {
+        $converter = new Words();
+
+        return $converter->transformToCurrency($amount, 'ru', $currency);
+    }
+}
