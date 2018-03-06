b08257d8df506e798771ed58a9f285a6525d25c5
iamdual <ekin@kodigen.com>
2018-03-06 22:52:51 +0300

    Fixing grammar for TR
    Now, it working more smoothly than previous version.
    
    Example outputs:
    102100 => yüz iki bin yüz
    1001 => bin bir
    1000 => bin
    1020 => bin yirmi
    98320561 => doksan sekiz milyon üç yüz yirmi bin beş yüz altmış bir

--- src/Legacy/Numbers/Words/Locale/Tr.php
+++ src/Legacy/Numbers/Words/Locale/Tr.php
@@ -14,12 +14,28 @@ class Tr extends Words
     private $minus = 'eksi';
 
     private static $exponent = [
-        0  => [''],
-        3  => ['bin'],
-        6  => ['milyon'],
-        12 => ['milyar'],
-        18 => ['trilyon'],
-        24 => ['katrilyon'],
+        '',
+        'bin',
+        'milyon',
+        'milyar',
+        'trilyon',
+        'katrilyon',
+        'kuintrilyon',
+        'seksilyon',
+        'septrilyon',
+        'oktrilyon',
+        'nanilyon',
+        'desilyon',
+        'andesilyon',
+        'dudesilyon',
+        'tredesilyon',
+        'kattırdesilyon',
+        'kuindesilyon',
+        'seksdesilyon',
+        'septendesilyon',
+        'oktadesilyon',
+        'novemdesilyon',
+        'vijintilyon'
     ];
 
     protected static $digits = [
@@ -33,6 +49,20 @@ class Tr extends Words
         'yedi',
         'sekiz',
         'dokuz'
+    ];
+
+    protected static $digits_second = [
+        '',
+        'on',
+        'yirmi',
+        'otuz',
+        'kırk',
+        'elli',
+        'altmış',
+        'yetmiş',
+        'seksen',
+        'doksan',
+        'yüz'
     ];
 
     private $wordSeparator = ' ';
@@ -84,126 +114,42 @@ class Tr extends Words
      *
      * @return string
      */
-    protected function toWords($num, $power = 0)
+    protected function toWords($num)
     {
-        // The return string;
         $ret = '';
+        $num = strval($num);
 
-        // add a the word for the minus sign if necessary
         if (substr($num, 0, 1) == '-') {
-            $ret = $this->wordSeparator . $this->minus;
+            $ret = $this->minus . $this->wordSeparator;
             $num = substr($num, 1);
         }
 
-
-        // strip excessive zero signs
         $num = preg_replace('/^0+/', '', $num);
+        $num_length = strlen($num);
 
-        if (strlen($num) > 6) {
-            $current_power = 6;
-            // check for highest power
-            if (isset(self::$exponent[$power])) {
-                // convert the number above the first 6 digits
-                // with it's corresponding $power.
-                $snum = substr($num, 0, -6);
-                $snum = preg_replace('/^0+/', '', $snum);
-                if ($snum !== '') {
-                    $ret .= $this->toWords($snum, $power + 6);
-                }
-            }
-            $num = substr($num, -6);
-            if ($num == 0) {
-                return $ret;
-            }
-        } elseif ($num == 0 || $num == '') {
-            return (' ' . self::$digits[0] . ' ');
-            $current_power = strlen($num);
-        } else {
-            $current_power = strlen($num);
+        if ($num_length % 3 !== 0) {
+            $num = str_pad($num, $num_length + (3-($num_length%3)), '0', STR_PAD_LEFT);
         }
 
-        // See if we need "thousands"
-        $thousands = floor($num / 1000);
-        if ($thousands == 1) {
-            $ret .= $this->wordSeparator . 'bin' . $this->wordSeparator;
-        } elseif ($thousands > 1) {
-            $ret .= $this->toWords($thousands, 3) . $this->wordSeparator;//. 'mil' . $this->wordSeparator;
-        }
+        $groups = str_split($num, 3);
+        $g_index = count($groups) - 1;
+        foreach ($groups as $i => $g) {
 
-        // values for digits, tens and hundreds
-        $h = floor(($num / 100) % 10);
-        $t = floor(($num / 10) % 10);
-        $d = floor($num % 10);
+            if ((int) $g[0] > 1)
+                $ret .= self::$digits[$g[0]] . $this->wordSeparator;
 
-        if ($h) {
-            $ret .= $this->wordSeparator . self::$digits[$h] . $this->wordSeparator . 'yüz';
+            if ((int) $g[0] > 0)
+                $ret .= "yüz" . $this->wordSeparator;
 
-            // in English only - add ' and' for [1-9]01..[1-9]99
-            // (also for 1001..1099, 10001..10099 but it is harder)
-            // for now it is switched off, maybe some language purists
-            // can force me to enable it, or to remove it completely
-            // if (($t + $d) > 0)
-            //   $ret .= $this->wordSeparator . 'and';
-        }
+            if ((int) $g[1] > 0)
+                $ret .= self::$digits_second[$g[1]] . $this->wordSeparator;
 
-        // decine: venti trenta, etc...
-        switch ($t) {
-            case 9:
-                $ret .= $this->wordSeparator . 'doksan';
-                break;
+            if ((int) $g[2] > 0 && (($num_length === 4 && $i === 0 && $g[2] <= 1) === false))
+                $ret .= self::$digits[$g[2]] . $this->wordSeparator;
 
-            case 8:
-                $ret .= $this->wordSeparator . 'seksen';
-                break;
+            $ret .= self::$exponent[$g_index] . $this->wordSeparator;
 
-            case 7:
-                $ret .= $this->wordSeparator . 'yetmiş';
-                break;
-
-            case 6:
-                $ret .= $this->wordSeparator . 'altmış';
-                break;
-
-            case 5:
-                $ret .= $this->wordSeparator . 'elli';
-                break;
-
-            case 4:
-                $ret .= $this->wordSeparator . 'kırk';
-                break;
-
-            case 3:
-                $ret .= $this->wordSeparator . 'otuz';
-                break;
-
-            case 2:
-                $ret .= $this->wordSeparator . 'yirmi';
-                break;
-
-            case 2:
-                $ret .= $this->wordSeparator . 'on';
-                break;
-
-                break;
-        }
-
-        if ($t > 1 && $d > 0) {
-            $ret .= $this->wordSeparator . self::$digits[$d];
-        }
-
-        if ($power > 0) {
-            if (isset(self::$exponent[$power])) {
-                $lev = self::$exponent[$power];
-            }
-
-            if (!isset($lev) || !is_array($lev)) {
-                return null;
-            }
-
-            $suffix = $lev[0];
-            if ($num != 0) {
-                $ret .= $this->wordSeparator . $suffix;
-            }
+            $g_index--;
         }
 
         return $ret;
