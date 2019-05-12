8aa794086f7a71a0943b438a90ed3cebac004ad3
Xeux Innovations <info@xeux.com.pa>
2019-05-12 12:19:22 -0500

    Update SpanishCurrencyTransformerTest.php
    Tests for previous commit

--- tests/CurrencyTransformer/SpanishCurrencyTransformerTest.php
+++ tests/CurrencyTransformer/SpanishCurrencyTransformerTest.php
@@ -12,13 +12,15 @@ class SpanishCurrencyTransformerTest extends CurrencyTransformerTest
     public function providerItConvertsMoneyAmountToWords()
     {
         return [
-            [100, 'PLN', 'uno zloty'],
+            [100, 'PAB', 'un balboa'],
+            [100, 'PLN', 'un zloty'],
+            [200, 'PAB', 'dos balboas'],
             [200, 'PLN', 'dos zlotys'],
             [500, 'EUR', 'cinco euros'],
+            [52481, 'CZK', 'quinientos veinticuatro czech korunas con ochenta y uno halerzs'],
             [61500, 'NOK', 'seiscientos quince norwegian krones'],
             [154552, 'USD', 'mil quinientos cuarenta y cinco dólares con cincuenta y dos centavos'],
             [304501, 'EUR', 'tres mil cuarenta y cinco euros con uno centavo'],
-            [52481, 'CZK', 'quinientos veinticuatro czech korunas con ochenta y uno halerzs'],
         ];
     }
 }
