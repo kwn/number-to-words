3c0a70883e194e116684978f291a1c7e3e9ef3ff
coupej <jeroencoupe@gmail.com>
2019-07-15 20:08:45 +0200

    Improve support for dutch language (#81)
    * Improve support for dutch language
    
    * added special case combinaton 1000+ combined with number <= 12

--- src/Legacy/Numbers/Words/Locale/Nl.php
+++ src/Legacy/Numbers/Words/Locale/Nl.php
@@ -49,7 +49,7 @@ class Nl extends Words
 
     private static $digits = [
         'nul',
-        'een',
+        'één',
         'twee',
         'drie',
         'vier',
@@ -113,6 +113,7 @@ class Nl extends Words
     protected function toWords($num, $power = 0, $powsuffix = '')
     {
         $ret = '';
+        $hasPower =false;; 
 
         // add a minus sign
         if (substr($num, 0, 1) == '-') {
@@ -123,7 +124,6 @@ class Nl extends Words
         // strip excessive zero signs and spaces
         $num = trim($num);
         $num = preg_replace('/^0+/', '', $num);
-
         if (strlen($num) > 3) {
             $maxp = strlen($num) - 1;
             $curp = $maxp;
@@ -139,7 +139,7 @@ class Nl extends Words
                         if ($powsuffix != '') {
                             $cursuffix .= $this->wordSeparator . $powsuffix;
                         }
-
+                        $hasPower = true;
                         $ret .= $this->toWords($snum, $p, $cursuffix);
                     }
                     $curp = $p - 1;
@@ -173,37 +173,40 @@ class Nl extends Words
         }
 
         if ($h) {
-            $ret .= $this->wordSeparator . self::$digits[$h] . $this->wordSeparator . 'honderd';
+            $ret .= $this->wordSeparator .  ($h==1?'':self::$digits[$h]) . $this->wordSeparator . 'honderd';
         }
-
+        // an number under 12 with wants en if it is substituted 
+        if ($hasPower && $num <= 12 && $num > 0)
+        {
+            $ret .= 'en ';
+        }
         // add digits only in <0>,<1,9> and <21,inf>
-        if ($t != 1 && $d > 0) {
-            if ($t > 0) {
+        if ( $d > 0) {
+            if ($t != 1 && $t > 0) {
                 $ret .= self::$digits[$d] . 'en';
             } else {
-                $ret .= self::$digits[$d];
-                if ($d == 1) {
-                    if ($power == 0) {
-                        $ret .= 's'; // fuer eins
-                    } else {
-                        if ($power != 3) {  // tausend ausnehmen
-                            $ret .= ''; // fuer eine
-                        }
-                    }
+                // 100 en 9 of 100 en 12 maar honderddertien 
+                if ( $h > 0 && ($t == 0 || ($t== 1 && ($d==1 || $d==2))))
+                {
+                    $ret .= ' en ';
+                } 
+                if($t != 1 && !($d ==1 && $power == 3 )){
+                    $ret .= self::$digits[$d];
                 }
             }
         }
 
         // ten, twenty etc.
         switch ($t) {
-            case 9:
             case 8:
+                $ret .= $this->wordSeparator . 't'.self::$digits[$t] . 'ig';
+                break;
+            case 9:
             case 7:
             case 6:
             case 5:
                 $ret .= $this->wordSeparator . self::$digits[$t] . 'tig';
                 break;
-
             case 4:
                 $ret .= $this->wordSeparator . 'veertig';
                 break;
@@ -257,9 +260,8 @@ class Nl extends Words
             if (!isset($lev) || !is_array($lev)) {
                 return null;
             }
-
             if ($power == 3) {
-                $ret .= $this->wordSeparator . $lev[0];
+                $ret .= $this->wordSeparator . $lev[0] . self::$exponentWordSeparator;
             } elseif ($d == 1 && ($t + $h) == 0) {
                 $ret .= self::$exponentWordSeparator . $lev[0] . self::$exponentWordSeparator;
             } else {
