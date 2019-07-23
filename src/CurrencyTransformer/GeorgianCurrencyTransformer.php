994e2ac5e78e3bb9c933a3c9c861138ba03e2abb
godot <godo72310@gmail.com>
2019-07-23 12:25:28 +0400

    +

--- src/CurrencyTransformer/GeorgianCurrencyTransformer.php
+++ src/CurrencyTransformer/GeorgianCurrencyTransformer.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: goderdzi.jobadze
- * Date: 7/19/2019
- * Time: 2:52 PM
- */
 
 namespace NumberToWords\CurrencyTransformer;
-
 
 use NumberToWords\Legacy\Numbers\Words;
 use NumberToWords\TransformerOptions\CurrencyTransformerOptions;
@@ -15,16 +8,10 @@ use NumberToWords\TransformerOptions\CurrencyTransformerOptions;
 class GeorgianCurrencyTransformer implements CurrencyTransformer
 {
 
-    /**
-     * @param int $amount
-     * @param string $currency
-     * @param CurrencyTransformerOptions|null $options
-     *
-     * @return string
-     */
     public function toWords($amount, $currency, $options = null)
     {
         $converter = new Words($options);
         return $converter->transformToCurrency($amount, 'ka', $currency);
+
     }
 }
--- src/NumberTransformer/GeorgianNumberTransformer.php
+++ src/NumberTransformer/GeorgianNumberTransformer.php
@@ -1,28 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: goderdzi.jobadze
- * Date: 7/19/2019
- * Time: 2:50 PM
- */
-
 namespace NumberToWords\NumberTransformer;
-
 
 use NumberToWords\Legacy\Numbers\Words;
 
 class GeorgianNumberTransformer implements NumberTransformer
 {
 
-    /**
-     * @param int $number
-     *
-     * @return string
-     */
     public function toWords($number)
     {
         $converter = new Words();
         return $converter->transformToWords($number, 'ka');
 
     }
+
 }
