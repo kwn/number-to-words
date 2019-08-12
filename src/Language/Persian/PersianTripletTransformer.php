a71749b615825ed4363e97d7dafcd5165a02f460
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-08-12 23:39:07 +0430

    language dictionary

---
+++ src/Language/Persian/PersianTripletTransformer.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace NumberToWords\Language\Persian;
+
+use NumberToWords\Language\TripletTransformer;
+
+class PersianTripletTransformer implements TripletTransformer
+{
+    /**
+     * @var EnglishDictionary
+     */
+    private $dictionary;
+
+    /**
+     * @param EnglishDictionary $dictionary
+     */
+    public function __construct(PersianDictionary $dictionary)
+    {
+        $this->dictionary = $dictionary;
+    }
+
+    /**
+     * @param int $number
+     *
+     * @return string
+     */
+    public function transformToWords($number)
+    {
+        $units = $number % 10;
+        $tens = (int) ($number / 10) % 10;
+        $hundreds = (int) ($number / 100) % 10;
+        $words = [];
+
+        if ($hundreds > 0) {
+            $words[] = $this->dictionary->getCorrespondingHundred($hundreds);
+        }
+
+        if ($tens !== 0 || $units !== 0) {
+            $words[] = $this->getSubHundred($tens, $units);
+        }
+
+        return implode(' ', $words);
+    }
+
+    /**
+     * @param int $tens
+     * @param int $units
+     *
+     * @return string
+     */
+    private function getSubHundred($tens, $units)
+    {
+        $words = [];
+
+        if ($tens === 1) {
+            $words[] = $this->dictionary->getCorrespondingTeen($units);
+        } else {
+            if ($tens > 0) {
+                $words[] = $this->dictionary->getCorrespondingTen($tens);
+            }
+            if ($units > 0) {
+                $words[] = $this->dictionary->getCorrespondingUnit($units);
+            }
+        }
+
+        return implode('-', $words);
+    }
+}
