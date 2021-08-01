01fa74606531ca5c8a0896c52b056b21d47803f4
Cristian Gomez <thepany96@gmail.com>
2021-07-31 20:58:08 -0400

    fix travis build

--- src/ManagesCurrencyTransformers.php
+++ src/ManagesCurrencyTransformers.php
@@ -2,29 +2,47 @@
 
 namespace NumberToWords;
 
-use NumberToWords\CurrencyTransformer;
+use NumberToWords\CurrencyTransformer\GeorgianCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\GermanCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\HungarianCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\DanishCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\EnglishCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\LatvianCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\LithuanianCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\PolishCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\PortugueseBrazilianCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\RomanianCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\RussianCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\SlovakCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\SpanishCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\TurkmenCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\TurkishCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\UkrainianCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\FrenchCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\YorubaCurrencyTransformer;
+use NumberToWords\CurrencyTransformer\AlbanianCurrencyTransformer;
 
 trait ManagesCurrencyTransformers
 {
     private array $currencyTransformers = [
-        'al' => CurrencyTransformer\AlbanianCurrencyTransformer::class,
-        'de' => CurrencyTransformer\GermanCurrencyTransformer::class,
-        'dk' => CurrencyTransformer\DanishCurrencyTransformer::class,
-        'en' => CurrencyTransformer\EnglishCurrencyTransformer::class,
-        'es' => CurrencyTransformer\SpanishCurrencyTransformer::class,
-        'fr' => CurrencyTransformer\FrenchCurrencyTransformer::class,
-        'hu' => CurrencyTransformer\HungarianCurrencyTransformer::class,
-        'ka' => CurrencyTransformer\GeorgianCurrencyTransformer::class,
-        'lt' => CurrencyTransformer\LithuanianCurrencyTransformer::class,
-        'lv' => CurrencyTransformer\LatvianCurrencyTransformer::class,
-        'pl' => CurrencyTransformer\PolishCurrencyTransformer::class,
-        'pt_BR' => CurrencyTransformer\PortugueseBrazilianCurrencyTransformer::class,
-        'ro' => CurrencyTransformer\RomanianCurrencyTransformer::class,
-        'ru' => CurrencyTransformer\RussianCurrencyTransformer::class,
-        'sk' => CurrencyTransformer\SlovakCurrencyTransformer::class,
-        'tk' => CurrencyTransformer\TurkmenCurrencyTransformer::class,
-        'tr' => CurrencyTransformer\TurkishCurrencyTransformer::class,
-        'ua' => CurrencyTransformer\UkrainianCurrencyTransformer::class,
-        'yo' => CurrencyTransformer\YorubaCurrencyTransformer::class
+        'al' => AlbanianCurrencyTransformer::class,
+        'de' => GermanCurrencyTransformer::class,
+        'dk' => DanishCurrencyTransformer::class,
+        'en' => EnglishCurrencyTransformer::class,
+        'es' => SpanishCurrencyTransformer::class,
+        'fr' => FrenchCurrencyTransformer::class,
+        'hu' => HungarianCurrencyTransformer::class,
+        'ka' => GeorgianCurrencyTransformer::class,
+        'lt' => LithuanianCurrencyTransformer::class,
+        'lv' => LatvianCurrencyTransformer::class,
+        'pl' => PolishCurrencyTransformer::class,
+        'pt_BR' => PortugueseBrazilianCurrencyTransformer::class,
+        'ro' => RomanianCurrencyTransformer::class,
+        'ru' => RussianCurrencyTransformer::class,
+        'sk' => SlovakCurrencyTransformer::class,
+        'tk' => TurkmenCurrencyTransformer::class,
+        'tr' => TurkishCurrencyTransformer::class,
+        'ua' => UkrainianCurrencyTransformer::class,
+        'yo' => YorubaCurrencyTransformer::class
     ];
 }
--- src/ManagesNumberTransformers.php
+++ src/ManagesNumberTransformers.php
@@ -2,39 +2,67 @@
 
 namespace NumberToWords;
 
-use NumberToWords\NumberTransformer;
+use NumberToWords\NumberTransformer\BulgarianNumberTransformer;
+use NumberToWords\NumberTransformer\CzechNumberTransformer;
+use NumberToWords\NumberTransformer\DanishNumberTransformer;
+use NumberToWords\NumberTransformer\DutchNumberTransformer;
+use NumberToWords\NumberTransformer\EnglishNumberTransformer;
+use NumberToWords\NumberTransformer\EstonianNumberTransformer;
+use NumberToWords\NumberTransformer\FrenchBelgianNumberTransformer;
+use NumberToWords\NumberTransformer\FrenchNumberTransformer;
+use NumberToWords\NumberTransformer\GeorgianNumberTransformer;
+use NumberToWords\NumberTransformer\GermanNumberTransformer;
+use NumberToWords\NumberTransformer\HungarianNumberTransformer;
+use NumberToWords\NumberTransformer\IndonesianNumberTransformer;
+use NumberToWords\NumberTransformer\ItalianNumberTransformer;
+use NumberToWords\NumberTransformer\LatvianNumberTransformer;
+use NumberToWords\NumberTransformer\LithuanianNumberTransformer;
+use NumberToWords\NumberTransformer\MalayNumberTransformer;
+use NumberToWords\NumberTransformer\PersianNumberTransformer;
+use NumberToWords\NumberTransformer\PolishNumberTransformer;
+use NumberToWords\NumberTransformer\PortugueseBrazilianNumberTransformer;
+use NumberToWords\NumberTransformer\RomanianNumberTransformer;
+use NumberToWords\NumberTransformer\RussianNumberTransformer;
+use NumberToWords\NumberTransformer\SlovakNumberTransformer;
+use NumberToWords\NumberTransformer\SpanishNumberTransformer;
+use NumberToWords\NumberTransformer\SwedishNumberTransformer;
+use NumberToWords\NumberTransformer\TurkishNumberTransformer;
+use NumberToWords\NumberTransformer\TurkmenNumberTransformer;
+use NumberToWords\NumberTransformer\UkrainianNumberTransformer;
+use NumberToWords\NumberTransformer\YorubaNumberTransformer;
+use NumberToWords\NumberTransformer\AlbanianNumberTransformer;
 
 trait ManagesNumberTransformers
 {
     private array $numberTransformers = [
-        'al' => NumberTransformer\AlbanianNumberTransformer::class,
-        'bg' => NumberTransformer\BulgarianNumberTransformer::class,
-        'cs' => NumberTransformer\CzechNumberTransformer::class,
-        'de' => NumberTransformer\GermanNumberTransformer::class,
-        'dk' => NumberTransformer\DanishNumberTransformer::class,
-        'en' => NumberTransformer\EnglishNumberTransformer::class,
-        'es' => NumberTransformer\SpanishNumberTransformer::class,
-        'et' => NumberTransformer\EstonianNumberTransformer::class,
-        'fa' => NumberTransformer\PersianNumberTransformer::class,
-        'fr' => NumberTransformer\FrenchNumberTransformer::class,
-        'fr_BE' => NumberTransformer\FrenchBelgianNumberTransformer::class,
-        'hu' => NumberTransformer\HungarianNumberTransformer::class,
-        'id' => NumberTransformer\IndonesianNumberTransformer::class,
-        'it' => NumberTransformer\ItalianNumberTransformer::class,
-        'ka' => NumberTransformer\GeorgianNumberTransformer::class,
-        'lt' => NumberTransformer\LithuanianNumberTransformer::class,
-        'lv' => NumberTransformer\LatvianNumberTransformer::class,
-        'ms' => NumberTransformer\MalayNumberTransformer::class,
-        'nl' => NumberTransformer\DutchNumberTransformer::class,
-        'pl' => NumberTransformer\PolishNumberTransformer::class,
-        'pt_BR' => NumberTransformer\PortugueseBrazilianNumberTransformer::class,
-        'ro' => NumberTransformer\RomanianNumberTransformer::class,
-        'ru' => NumberTransformer\RussianNumberTransformer::class,
-        'sk' => NumberTransformer\SlovakNumberTransformer::class,
-        'sv' => NumberTransformer\SwedishNumberTransformer::class,
-        'tk' => NumberTransformer\TurkmenNumberTransformer::class,
-        'tr' => NumberTransformer\TurkishNumberTransformer::class,
-        'ua' => NumberTransformer\UkrainianNumberTransformer::class,
-        'yo' => NumberTransformer\YorubaNumberTransformer::class,
+        'al' => AlbanianNumberTransformer::class,
+        'bg' => BulgarianNumberTransformer::class,
+        'cs' => CzechNumberTransformer::class,
+        'de' => GermanNumberTransformer::class,
+        'dk' => DanishNumberTransformer::class,
+        'en' => EnglishNumberTransformer::class,
+        'es' => SpanishNumberTransformer::class,
+        'et' => EstonianNumberTransformer::class,
+        'fa' => PersianNumberTransformer::class,
+        'fr' => FrenchNumberTransformer::class,
+        'fr_BE' => FrenchBelgianNumberTransformer::class,
+        'hu' => HungarianNumberTransformer::class,
+        'id' => IndonesianNumberTransformer::class,
+        'it' => ItalianNumberTransformer::class,
+        'ka' => GeorgianNumberTransformer::class,
+        'lt' => LithuanianNumberTransformer::class,
+        'lv' => LatvianNumberTransformer::class,
+        'ms' => MalayNumberTransformer::class,
+        'nl' => DutchNumberTransformer::class,
+        'pl' => PolishNumberTransformer::class,
+        'pt_BR' => PortugueseBrazilianNumberTransformer::class,
+        'ro' => RomanianNumberTransformer::class,
+        'ru' => RussianNumberTransformer::class,
+        'sk' => SlovakNumberTransformer::class,
+        'sv' => SwedishNumberTransformer::class,
+        'tk' => TurkmenNumberTransformer::class,
+        'tr' => TurkishNumberTransformer::class,
+        'ua' => UkrainianNumberTransformer::class,
+        'yo' => YorubaNumberTransformer::class,
     ];
 }
