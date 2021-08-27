6620a450477633dbaec7dd8956720ad5de7a6cbc
m.habibian <mojtabaa.hn@gmail.com>
2021-08-27 10:29:56 +0430

    Fixes persian number converter

--- src/NumberTransformer/PersianNumberTransformer.php
+++ src/NumberTransformer/PersianNumberTransformer.php
@@ -2,27 +2,14 @@
 
 namespace NumberToWords\NumberTransformer;
 
-use NumberToWords\Service\NumberToTripletsConverter;
+use NumberToWords\Language\Persian\PersianConverter;
 use NumberToWords\Language\Persian\PersianDictionary;
-use NumberToWords\Language\Persian\PersianExponentGetter;
-use NumberToWords\Language\Persian\PersianTripletTransformer;
 
 class PersianNumberTransformer implements NumberTransformer
 {
     public function toWords(int $number): string
     {
         $dictionary = new PersianDictionary();
-        $numberToTripletsConverter = new NumberToTripletsConverter();
-        $tripletTransformer = new PersianTripletTransformer($dictionary);
-        $exponentInflector = new PersianExponentGetter();
-
-        $numberTransformer = (new NumberTransformerBuilder())
-            ->withDictionary($dictionary)
-            ->withWordsSeparatedBy(' ')
-            ->transformNumbersBySplittingIntoTriplets($numberToTripletsConverter, $tripletTransformer)
-            ->useRegularExponents($exponentInflector)
-            ->build();
-
-        return $numberTransformer->toWords($number);
+        return (new PersianConverter($dictionary))->convert($number);
     }
 }
