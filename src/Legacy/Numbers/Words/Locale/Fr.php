ac4e1887951d2a7a22f824f85957f2b64ae1e8da
Ange <45317331+Ange0@users.noreply.github.com>
2022-07-06 12:45:52 +0000

    Update Fr.php
    Add GBP on french locale

--- src/Legacy/Numbers/Words/Locale/Fr.php
+++ src/Legacy/Numbers/Words/Locale/Fr.php
@@ -97,6 +97,7 @@ class Fr extends Words
         'CNY' => [['yuan'], ['fen']],
         'DZD' => [['dinar'], ['centime']],
         'EUR' => [['euro'], ['centime']],
+        'GBP' => [['livre sterling', 'livres sterling'], ['penny', 'pence']],
         'JPY' => [['yen', ['sen']]],
         'LYD' => [['dinar'], ['centime']],
         'MAD' => [['dirham'], ['centime']],
