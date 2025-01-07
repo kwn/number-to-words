c1fede59c3494c2c99b7d8d9ff7242cf89d36db7
ayrus02 <34953163+ayrus02@users.noreply.github.com>
2025-01-08 01:42:09 +0700

    Bug Fix: Add Indonesian and Malaysian Currency Transformer (Reopen) (#184)
    * Update ManagesCurrencyTransformers.php
    
    Add Indonesian Currency Transformer
    
    * Update ManagesCurrencyTransformers.php
    
    Add Malaysian Currency Transformer
    
    * Update EnglishDictionary.php
    
    Add Indonesian Rupiah and Malaysian Ringgit to EnglishDictionary.php
    
    * Add Indonesian Rupiah and Malaysian Ringgit Unit Test
    
    * Fix the mistaken unit test
    
    * Fix first letter capital in Rupiah IDR and Ringgit MYR

--- tests/CurrencyTransformer/EnglishCurrencyTransformerTest.php
+++ tests/CurrencyTransformer/EnglishCurrencyTransformerTest.php
@@ -34,6 +34,16 @@ class EnglishCurrencyTransformerTest extends CurrencyTransformerTest
             [-72925, 'USD', 'minus seven hundred twenty-nine dollars twenty-five cents'],
             [-89425, 'USD', 'minus eight hundred ninety-four dollars twenty-five cents'],
             [-99925, 'USD', 'minus nine hundred ninety-nine dollars twenty-five cents'],
+            [70800000, 'IDR', 'seven hundred eight thousand rupiahs'],
+            [276037213, 'IDR', 'two million seven hundred sixty thousand three hundred seventy-two rupiahs thirteen sens'],
+            [857998800, 'IDR', 'eight million five hundred seventy-nine thousand nine hundred eighty-eight rupiahs'],
+            [2227500000, 'IDR', 'twenty-two million two hundred seventy-five thousand rupiahs'],
+            [-191418500, 'IDR', 'minus one million nine hundred fourteen thousand one hundred eighty-five rupiahs'],
+            [7200000, 'MYR', 'seventy-two thousand ringgits'],
+            [177537065, 'MYR', 'one million seven hundred seventy-five thousand three hundred seventy ringgits sixty-five sens'],
+            [247314700, 'MYR', 'two million four hundred seventy-three thousand one hundred forty-seven ringgits'],
+            [980000000, 'MYR', 'nine million eight hundred thousand ringgits'],
+            [-135522300, 'MYR', 'minus one million three hundred fifty-five thousand two hundred twenty-three ringgits'],
         ];
     }
 }
