982c8f6e96ec981ed666b12b1aeee0bebdc802d4
Milen Dzhambazov <milen.dzhambazov@devision.com>
2025-03-17 21:34:11 +0200

    fixed a corner case in BG lang to handle one quantity of a thousand properly

--- src/Language/Bulgarian/BulgarianTripletTransformer.php
+++ src/Language/Bulgarian/BulgarianTripletTransformer.php
@@ -42,7 +42,11 @@ class BulgarianTripletTransformer implements PowerAwareTripletTransformer, Gramm
         }
 
         if ($units > 0 && $tens !== 1) {
-            // Skip "one" in one thousand because in Bulgarian it's not used
+            /**
+             * Handles one quantity of a thousand by omitting the quantifier (one) to comply with Bulgarian grammar
+             * Example #1: 1001345 => един милион хиляда триста четиридесет и пет лева и нула стотинки
+             * Example #2: 1002345 => един милион две хиляди триста четиридесет и пет лева и нула стотинки
+             */
             if ($power == 1 && $units === 1 && $hundreds === 0 && $tens === 0) {
                 return null;
             } else {
